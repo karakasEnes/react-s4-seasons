0a8a10553121d146f9a6154d6cc730a873f25c2e
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-06 15:26:24 +0300

    show icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -10,10 +10,16 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === "winter" ? "Burry It's Cold" : "Lets hit the Beach";
+  const icon = season === "winter" ? "snowflake" : "sun";
 
-  console.log(season);
-
-  return <div>SeasonDisplay</div>;
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
